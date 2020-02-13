c5396d6d5366fd81d4b5649eb8664db3f9f990d2
Jere Suikkila <jere.suikkila@valuemotive.com>
2020-02-13 17:52:17 +0200

    Add subnet designation

--- main.tf
+++ main.tf
@@ -41,6 +41,7 @@ resource "aws_instance" "wireguard" {
   key_name                    = var.ssh_key_id
   iam_instance_profile        = aws_iam_instance_profile.wireguard_profile[0].name
   user_data                   = data.template_file.user_data.rendered
+  subnet_id                   = var.subnet_ids[0]
   vpc_security_group_ids      = [aws_security_group.sg_wireguard_external.id]
   associate_public_ip_address = true
 
