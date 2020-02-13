c6bf1bf0508edf85a0364cb205e35aa0c2384d96
Jere Suikkila <jere.suikkila@valuemotive.com>
2020-02-13 16:08:05 +0200

    Always use elastic ip

--- wireguard-iam.tf
+++ wireguard-iam.tf
@@ -22,10 +22,11 @@ data "aws_iam_policy_document" "wireguard_policy_doc" {
 }
 
 resource "aws_iam_policy" "wireguard_policy" {
-  name               = "tf-wireguard-${var.env}"
-  description        = "Terraform Managed. Allows Wireguard instance to attach EIP."
-  policy             = data.aws_iam_policy_document.wireguard_policy_doc.json
-  count              = (var.eip_id != "disabled" ? 1 : 0) # only used for EIP mode
+  name        = "tf-wireguard-${var.env}"
+  description = "Terraform Managed. Allows Wireguard instance to attach EIP."
+  policy      = data.aws_iam_policy_document.wireguard_policy_doc.json
+  # count              = (var.eip_id != "disabled" ? 1 : 0) # only used for EIP mode
+  count = 1
 }
 
 resource "aws_iam_role" "wireguard_role" {
@@ -33,17 +34,20 @@ resource "aws_iam_role" "wireguard_role" {
   description        = "Terraform Managed. Role to allow Wireguard instance to attach EIP."
   path               = "/"
   assume_role_policy = data.aws_iam_policy_document.ec2_assume_role.json
-  count              = (var.eip_id != "disabled" ? 1 : 0) # only used for EIP mode
+  # count              = (var.eip_id != "disabled" ? 1 : 0) # only used for EIP mode
+  count = 1
 }
 
 resource "aws_iam_role_policy_attachment" "wireguard_roleattach" {
-  role               = aws_iam_role.wireguard_role[0].name
-  policy_arn         = aws_iam_policy.wireguard_policy[0].arn
-  count              = (var.eip_id != "disabled" ? 1 : 0) # only used for EIP mode
+  role       = aws_iam_role.wireguard_role[0].name
+  policy_arn = aws_iam_policy.wireguard_policy[0].arn
+  # count              = (var.eip_id != "disabled" ? 1 : 0) # only used for EIP mode
+  count = 1
 }
 
 resource "aws_iam_instance_profile" "wireguard_profile" {
-  name               = "tf-wireguard-${var.env}"
-  role               = aws_iam_role.wireguard_role[0].name
-  count              = (var.eip_id != "disabled" ? 1 : 0) # only used for EIP mode
+  name = "tf-wireguard-${var.env}"
+  role = aws_iam_role.wireguard_role[0].name
+  # count              = (var.eip_id != "disabled" ? 1 : 0) # only used for EIP mode
+  count = 1
 }
